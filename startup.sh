c2690e9e5146a99129091f94f66e295f966ccb62
Dheeraj <117371624+Dheeraj0-0@users.noreply.github.com>
2025-07-01 09:25:42 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "📦 Installing packages..."
+echo "🐍 Starting custom startup.sh"
+echo "📦 Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt --target="./.python_packages/lib/site-packages"
 
-echo "🚀 Launching Gunicorn on port 8000..."
+echo "🚀 Launching Gunicorn server..."
 PYTHONPATH="./.python_packages/lib/site-packages" gunicorn app:app --bind=0.0.0.0:8000
